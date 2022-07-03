af5e5798eebf2cbc565b3d35833142b31e1a9bc0
Riadh Habbachi <habbachi.riadh@med.tn>
2022-07-03 21:01:17 +0100

    feat(init.d/30-composer.sh): improve composer install command

--- docker/php/docker-entrypoint-init.d/30-composer.sh
+++ docker/php/docker-entrypoint-init.d/30-composer.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cd "$APP_ROOT" || exit 1
 
-composer install
-composer dump-autoload --optimize
+XDEBUG_MODE=off composer install --prefer-dist --optimize-autoloader --no-interaction --no-progress
